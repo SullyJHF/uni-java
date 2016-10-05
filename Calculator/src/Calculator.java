7f0df7948483a7c332ccf566318cc7d9a1d5e6f6
Sullivan Ford <sullyjhf@hotmail.co.uk>
2016-10-05 13:57:53 +0100

    Convert all indentation to 2 spaces

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -1,67 +1,67 @@
 public class Calculator {
-	int a, b, c;
-	
-	// Constructors
-	public Calculator(int a, int b, int c) {
-		this.a = a;
-		this.b = b;
-		this.c = c;
-	}
-	
-	public Calculator(int a, int b) {
-		this.a = a;
-		this.b = b;
-	}
-	
-	// Calculator methods
-	public int sum() {
-		return this.a + this.b;
-	}
-	
-	public int difference() {
-		return this.a - this.b;
-	}
+  int a, b, c;
 
-	public int product() {
-		return this.a * this.b;
-	}
-	
-	public float quotient() {
-		return (float)this.a / (float)this.b;
-	}
-	
-	public int modulus() {
-		return this.a % this.b;
-	}
-	
-	public double[] quadratic() {
-		double d = this.b * this.b - 4 * this.a * this.c;
-		if (d < 0){
-		   // System.out.println("Discriminant < 0, no real solutions" );
-		   return null;
-		}
+  // Constructors
+  public Calculator(int a, int b, int c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
 
-		double x1 = (-this.b - Math.sqrt(d)) / (2 * this.a);
-		double x2 = (-this.b + Math.sqrt(d)) / (2 * this.a);
-		double[] x = {x1, x2};
+  public Calculator(int a, int b) {
+    this.a = a;
+    this.b = b;
+  }
 
-		return x;
-	}
-	
-	// trig functions
-	public double cosine(int x) {
-		return Math.cos(degToRad(x));
-	}
-	
-	public double sine(int x) {
-		return Math.sin(degToRad(x));
-	}
-	
-	public double tangent(int x) {
-		return Math.tan(degToRad(x));
-	}
-	
-	private double degToRad(int x) {
-		return x * Math.PI / 180;
-	}
+  // Calculator methods
+  public int sum() {
+    return this.a + this.b;
+  }
+
+  public int difference() {
+    return this.a - this.b;
+  }
+
+  public int product() {
+    return this.a * this.b;
+  }
+
+  public float quotient() {
+    return (float) this.a / (float) this.b;
+  }
+
+  public int modulus() {
+    return this.a % this.b;
+  }
+
+  public double[] quadratic() {
+    double d = this.b * this.b - 4 * this.a * this.c;
+    if (d < 0) {
+      // System.out.println("Discriminant < 0, no real solutions" );
+      return null;
+    }
+
+    double x1 = (-this.b - Math.sqrt(d)) / (2 * this.a);
+    double x2 = (-this.b + Math.sqrt(d)) / (2 * this.a);
+    double[] x = { x1, x2 };
+
+    return x;
+  }
+
+  // trig functions
+  public double cosine(int x) {
+    return Math.cos(degToRad(x));
+  }
+
+  public double sine(int x) {
+    return Math.sin(degToRad(x));
+  }
+
+  public double tangent(int x) {
+    return Math.tan(degToRad(x));
+  }
+
+  private double degToRad(int x) {
+    return x * Math.PI / 180;
+  }
 }
