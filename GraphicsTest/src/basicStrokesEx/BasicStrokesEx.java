b58f4c59dcaeecf8025c522938d67aad5e6dbd5a
Sullivan Ford <sullyjhf@hotmail.co.uk>
2016-10-12 23:32:08 +0100

    Finish basic strokes example

--- GraphicsTest/src/basicStrokesEx/BasicStrokesEx.java
+++ GraphicsTest/src/basicStrokesEx/BasicStrokesEx.java
@@ -1,5 +1,32 @@
 package basicStrokesEx;
 
-public class BasicStrokesEx {
+import java.awt.EventQueue;
 
+import javax.swing.JFrame;
+
+public class BasicStrokesEx extends JFrame {
+  private static final long serialVersionUID = 3626660178058839841L;
+
+  public BasicStrokesEx() {
+    initUI();
+  }
+
+  private void initUI() {
+    add(new Surface());
+
+    setTitle("Basic strokes");
+    setSize(280, 270);
+    setLocationRelativeTo(null);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  }
+
+  public static void main(String[] args) {
+    EventQueue.invokeLater(new Runnable() {
+      @Override
+      public void run() {
+        BasicStrokesEx ex = new BasicStrokesEx();
+        ex.setVisible(true);
+      }
+    });
+  }
 }
--- GraphicsTest/src/basicStrokesEx/Surface.java
+++ GraphicsTest/src/basicStrokesEx/Surface.java
@@ -1,5 +1,47 @@
 package basicStrokesEx;
 
-public class Surface {
+import java.awt.BasicStroke;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
+import javax.swing.JPanel;
+
+public class Surface extends JPanel {
+  private static final long serialVersionUID = 2702759257707688468L;
+
+  private void doDrawing(Graphics g) {
+    Graphics2D g2d = (Graphics2D) g.create();
+
+    float[] dash1 = { 2f, 0f, 2f };
+    float[] dash2 = { 1f, 1f, 1f };
+    float[] dash3 = { 4f, 0f, 2f };
+    float[] dash4 = { 4f, 4f, 1f };
+
+    g2d.drawLine(20, 40, 250, 40);
+
+    BasicStroke bs1 = new BasicStroke(1, BasicStroke.CAP_BUTT, BasicStroke.JOIN_ROUND, 1.0f, dash1, 2f);
+    BasicStroke bs2 = new BasicStroke(1, BasicStroke.CAP_BUTT, BasicStroke.JOIN_ROUND, 1.0f, dash2, 2f);
+    BasicStroke bs3 = new BasicStroke(1, BasicStroke.CAP_BUTT, BasicStroke.JOIN_ROUND, 1.0f, dash3, 2f);
+    BasicStroke bs4 = new BasicStroke(1, BasicStroke.CAP_BUTT, BasicStroke.JOIN_ROUND, 1.0f, dash4, 2f);
+
+    g2d.setStroke(bs1);
+    g2d.drawLine(20, 80, 250, 80);
+
+    g2d.setStroke(bs2);
+    g2d.drawLine(20, 120, 250, 120);
+
+    g2d.setStroke(bs3);
+    g2d.drawLine(20, 160, 250, 160);
+
+    g2d.setStroke(bs4);
+    g2d.drawLine(20, 200, 250, 200);
+
+    g2d.dispose();
+  }
+  
+  @Override
+  public void paintComponent(Graphics g) {
+    super.paintComponent(g);
+    doDrawing(g);
+  }
 }
