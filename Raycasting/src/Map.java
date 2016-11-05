86bc3f2d0926d986ce00b32c332fb895a956ff1c
Sullivan Ford <sullyjhf@hotmail.co.uk>
2016-11-05 02:38:52 +0000

    Start working on image map integration

--- Raycasting/src/Map.java
+++ Raycasting/src/Map.java
@@ -1,4 +1,37 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class Map {
+  private File mapFile;
+  BufferedImage mapImg;
+  
+  private int w, h;
 
+  public Map(String filepath) {
+    this.mapFile = new File(filepath);
+    loadImage();
+    createArray();
+  }
+
+  private void loadImage() {
+    try {
+      mapImg = ImageIO.read(mapFile);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+  
+  private void createArray() {
+    this.w = mapImg.getWidth(null);
+    this.h = mapImg.getHeight(null);
+    System.out.printf("w: %d, h: %d", w, h);
+    for(int row = 0; row < this.w; row++) {
+      for(int cell = 0; cell < this.h; cell++) {
+        mapImg.getRGB(row, cell);
+      }
+    }
+  }
 }
