90d897bd641e1a87d79dda072c7a20dea3524aee
Sullivan Ford <sullyjhf@hotmail.co.uk>
2016-11-06 12:38:30 +0000

    Implement findClosestColour method

--- Raycasting/src/ColourHandler.java
+++ Raycasting/src/ColourHandler.java
@@ -3,6 +3,24 @@ import java.awt.Color;
 class ColourHandler {
   private int[] cInts = { -16777216, -14865581, -8510125, -16742575, -5549514, -10528945, -4013113, -3608, -65459,
       -23808, -5081, -16718794, -14045697, -8161636, -34904, -13142 };
+  private int[][] cRGBs = {
+      {0, 0, 0},
+      {29, 43, 83},
+      {126, 37, 83},
+      {0, 135, 81},
+      {171, 82, 54},
+      {95, 87, 79},
+      {194, 195, 199},
+      {255, 241, 232},
+      {255, 0, 77},
+      {255, 163, 0},
+      {255, 236, 39},
+      {0, 228, 54},
+      {41, 173, 255},
+      {131, 118, 156},
+      {255, 119, 168},
+      {255, 204, 170}
+  };
   private String[] cStrings = { "BLACK", "DARK_BLUE", "DARK_PURPLE", "DARK_GREEN", "BROWN", "DARK_GRAY", "LIGHT_GRAY",
       "WHITE", "RED", "ORANGE", "YELLOW", "GREEN", "BLUE", "INDIGO", "PINK", "PEACH" };
 
@@ -28,6 +46,22 @@ class ColourHandler {
     if (mapTile == 0) return null;
     mapTile--;
     return new Color(cInts[mapTile]);
+  }
+
+  public Color findClosestColor(int rgb) {
+    double smallestD = Double.MAX_VALUE;
+    int index = -1;
+    int r = (rgb >> 16) & 0xFF;
+    int g = (rgb >> 8) & 0xFF;
+    int b = rgb & 0xFF;
+    for(int i = 0; i < cInts.length; i++) {
+      double d = Math.pow(cRGBs[i][0] - r, 2) + Math.pow(cRGBs[i][1] - g, 2) + Math.pow(cRGBs[i][2] - b, 2);
+      if(d < smallestD) {
+        smallestD = d;
+        index = i;
+      }
+    }
+    return new Color(cInts[index]);
   }
 
   final Color BLACK = new Color(cInts[0]);
