7e2494813f2e51e831f2e49fdc62a90fddac5f5b
Sullivan Ford <sullyjhf@hotmail.co.uk>
2016-11-08 23:28:29 +0000

    Add SpriteSheet class

---
+++ Raycasting/src/SpriteSheet.java
@@ -0,0 +1,38 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class SpriteSheet {
+  private File file;
+  private BufferedImage image;
+
+  private int spriteSize;
+
+  private int w, h;
+
+  public SpriteSheet(String filepath) {
+    this.file = new File(filepath);
+    String fileName = this.file.getName();
+    this.spriteSize = Integer.valueOf(fileName.substring(0, fileName.indexOf('x')));
+    loadImage();
+    this.w = this.image.getWidth();
+    this.h = this.image.getHeight();
+  }
+
+  private void loadImage() {
+    try {
+      this.image = ImageIO.read(this.file);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public BufferedImage getSprite(int index) {
+    int x = (index * this.spriteSize) % this.w;
+    int y = (int) Math.floor((index * this.spriteSize) / this.w) * this.spriteSize;
+    BufferedImage sprite = this.image.getSubimage(x, y, x + this.spriteSize, y + this.spriteSize);
+    return sprite;
+  }
+}
