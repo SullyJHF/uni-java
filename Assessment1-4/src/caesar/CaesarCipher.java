e60b6c7f3d5dd4e110f36cbffb4c16fe44b399f7
Sullivan Ford <sullyjhf@hotmail.co.uk>
2016-11-27 20:32:47 +0000

    Add logic

---
+++ Assessment1-4/src/caesar/CaesarCipher.java
@@ -0,0 +1,34 @@
+package caesar;
+
+public class CaesarCipher {
+  private final int ALPHABET_LENGTH = 26;
+  private final int A_VALUE = Character.valueOf('A');
+  private int offset;
+
+  public CaesarCipher(int offset) {
+    setOffset(offset);
+  }
+
+  public String decryptString(String str) {
+    String tempStr = str.replaceAll(" ", "").toUpperCase();
+    String outputStr = new String();
+    for(char c : tempStr.toCharArray()) {
+      outputStr += decryptCharacter(c);
+    }
+    return outputStr;
+  }
+
+  public char decryptCharacter(char c) {
+    c = Character.toUpperCase(c);
+    int offsettedC = c - A_VALUE;
+    int newC = offsettedC - this.offset;
+    newC %= ALPHABET_LENGTH;
+    if(newC < 0) newC += ALPHABET_LENGTH;
+    newC += A_VALUE;
+    return (char) newC;
+  }
+
+  public void setOffset(int offset) {
+    this.offset = offset;
+  }
+}
