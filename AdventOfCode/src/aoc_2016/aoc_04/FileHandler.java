35f223a651f810862165bb298e13ddc965f8f4aa
Sullivan Ford <sullyjhf@hotmail.co.uk>
2016-12-16 11:18:18 +0000

    initial day 4

---
+++ AdventOfCode/src/aoc_2016/aoc_04/FileHandler.java
@@ -0,0 +1,32 @@
+package aoc_2016.aoc_04;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileHandler {
+  private Path path;
+  private StringBuilder input = new StringBuilder();
+
+  public FileHandler(String filepath) {
+    path = Paths.get("src/aoc_2016/aoc_04/res", filepath);
+    loadInput();
+  }
+
+  private void loadInput() {
+    try {
+      Files.lines(path).forEach(line -> input.append(line + "\n"));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public String getInput() {
+    return input.toString();
+  }
+
+  public String[] getInputsAsArray() {
+    return input.toString().split("\n");
+  }
+}
---
+++ AdventOfCode/src/aoc_2016/aoc_04/Main.java
@@ -0,0 +1,8 @@
+package aoc_2016.aoc_04;
+
+public class Main {
+  public static void main(String[] args) {
+    FileHandler fh = new FileHandler("input.txt");
+    System.out.println(fh.getInput());
+  }
+}
