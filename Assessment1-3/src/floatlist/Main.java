8f47b0724bc7f0aa9caa2061d195df42d80d8759
Sullivan Ford <sullyjhf@hotmail.co.uk>
2016-12-21 16:17:21 +0000

    Start assignment 3

---
+++ Assessment1-3/src/floatlist/Main.java
@@ -0,0 +1,5 @@
+package floatlist;
+
+public class Main {
+
+}
---
+++ Assessment1-3/src/floatlist/MultiFloat.java
@@ -0,0 +1,13 @@
+package floatlist;
+
+import java.util.LinkedList;
+
+public class MultiFloat {
+  public MultiFloat(LinkedList<Float> l) {
+
+  }
+
+  public MultiFloat(Float f) {
+
+  }
+}
