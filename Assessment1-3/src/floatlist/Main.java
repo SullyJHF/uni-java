78e9a098aeae7b82738ce867b79cdc0b36a0f2d4
Sullivan Ford <sullyjhf@hotmail.co.uk>
2016-12-24 23:12:57 +0000

    Start on main method

--- Assessment1-3/src/floatlist/Main.java
+++ Assessment1-3/src/floatlist/Main.java
@@ -1,5 +1,20 @@
 package floatlist;
 
-public class Main {
+import java.util.LinkedList;
+import java.util.Random;
 
+public class Main {
+  public static void main(String[] args) {
+    LinkedList<Float> floatList1 = new LinkedList<Float>();
+    LinkedList<Float> floatList2 = new LinkedList<Float>();
+    Random r = new Random();
+    int size1 = r.nextInt(100);
+    int size2 = r.nextInt(100);
+    for(int i = 0; i < size1; i++) {
+      floatList1.add(r.nextFloat());
+    }
+    for(int i = 0; i < size2; i++) {
+      floatList2.add(r.nextFloat());
+    }
+  }
 }
