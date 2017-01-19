49519f8c22339318c2ea1c99f9dead3ca5588c24
Sullivan Ford <sullyjhf@hotmail.co.uk>
2017-01-19 20:53:39 +0000

    Initialise variables and frame

--- Game Loop/src/game/Game.java
+++ Game Loop/src/game/Game.java
@@ -3,6 +3,22 @@ package game;
 import javax.swing.JFrame;
 
 public class Game extends JFrame implements Runnable {
+  private Thread thread;
+  private Screen screen;
+  private boolean running = false;
+
+  public Game() {
+    super("Game Loop");
+
+    screen = new Screen();
+    add(screen);
+
+    pack();
+    setLocationRelativeTo(null);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setVisible(true);
+  }
+
   @Override
   public void run() {
 
